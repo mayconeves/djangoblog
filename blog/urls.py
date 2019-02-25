96e29e86c6c0a15c71beb5ea6c9ef6fb51f2580f
Mayco Neves <mayco.neves@m4web.com.br>
2019-02-25 11:27:09 -0300

    Pastas2

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# atribuindo uma view chamada post_list à URL raiz
+	# atribuindo uma view chamada post_list à URL raiz 
 	path('', views.post_list, name='post_list'),
 ]
